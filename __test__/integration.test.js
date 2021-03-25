e159d727106b84f102f9722c7bea8732ffcbe16c
Elder Nogueira <elder.nogueirasilva1@gmail.com>
2021-03-25 17:51:18 -0300

    tests: token implemented in tests

--- __test__/integration.test.js
+++ __test__/integration.test.js
@@ -12,7 +12,7 @@ describe('POST/administrators/cadastro', () => {
     it ("should return 201 to register administrator", async() => {
         const response = await request(app)
         .post('/administrators/cadastro')
-        .send({ email: "teste@gmail.com", senha: "123mudar" })
+        .send({ email: "teste1@gmail.com", senha: "123mudar" })
 
     expect(response.status).toBe(201)
     })
@@ -22,17 +22,40 @@ describe('POST/administrators/login', () => {
     it ('should return 200 to login user', async() => {
         const response = await request(app)
         .post('/administrators/login')
-        .send({ email: "teste@gmail.com", senha: "123mudar" })
+        .send({ email: "teste1@gmail.com", senha: "123mudar" })
 
     expect(response.status).toBe(200)
     })
 })
+
+describe('PUT/administrators', () => {
+    it ('should return 200 to change user', async() => {
+        const response = await request(app)
+        .put('/administrators/2')
+        .send({ email: "teste2@gmail.com", senha: "123mudar" })
+        .set('authorization', `bearer ${token}`)
+
+    expect(response.status).toBe(200)
+    })
+})
+
+describe('DELETE/administrators', () => {
+    it ('should return 200 to delete user', async() => {
+        const response = await request(app)
+        .delete('/administrators/2')
+        .set('authorization', `bearer ${token}`)
+
+    expect(response.status).toBe(200)
+    })
+})
+
 
 // test of the users route
 describe('GET/users', () => {
     it ('should return 200 to listen the users', async () => {
         const response = await request(app)
         .get('/users')
+        .set('authorization', `bearer ${token}`)
 
     expect(response.status).toBe(200)
     });
@@ -42,6 +65,7 @@ describe('GET/users/:id', () => {
     it ('should return 200 to listen an user', async () => {
         const response = await request(app)
         .get('/users/29')
+        .set('authorization', `bearer ${token}`)
 
     expect(response.status).toBe(200)
     });
@@ -51,7 +75,8 @@ describe('POST/users', () => {
     it ('should return 201 to create an user', async () => {
         const response = await request(app)
         .post('/users')
-        .send({ "name": "João", "email": "joao1@hotmail.com"})
+        .send({ "name": "Marciel", "email": "joao1@hotmail.com"})
+        .set('authorization', `bearer ${token}`)
 
     expect(response.status).toBe(201)
     });
@@ -60,8 +85,9 @@ describe('POST/users', () => {
 describe('PUT/users/:id', () => {
     it ('should return 200 to change an user', async () => {
         const response = await request(app)
-        .put('/users/29')
-        .send({ "name": "João", "email": "joao20@hotmail.com"})
+        .put('/users/37')
+        .send({ "name": "Marcos", "email": "marquinho@hotmail.com"})
+        .set('authorization', `bearer ${token}`)
 
     expect(response.status).toBe(200)
     });
@@ -70,7 +96,8 @@ describe('PUT/users/:id', () => {
 describe('DELETE/users/:id', () => {
     it ('should return 200 to delete an user', async () => {
         const response = await request(app)
-        .delete('/users/34')
+        .delete('/users/36')
+        .set('authorization', `bearer ${token}`)
 
     expect(response.status).toBe(200)
     });
